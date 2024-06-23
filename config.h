c2677ce008b0722c6a90eda617a58dcfe5eb8280
B10101 <b37710101@gmail.com>
2024-06-23 21:58:44 +0300

    fin

---
+++ config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define WIN_INIT_W 1280
+#define WIN_INIT_H 720
+
+#define BG_COLOR (LfColor){1, 1, 1, 255}
+
+
+#define FONT "/home/bett/Documents/c/taskApp/Roboto_Mono/static/RobotoMono-MediumItalic.ttf"
+#define FONT_BOLD "/home/bett/Documents/c/taskApp/Roboto_Mono/static/RobotoMono-Bold.ttf"
+
+#define TODO_DATA_DIR getenv("HOME")
+#define TODO_DATA_FILE ".tododata"
+
+#define BACK_ICON "/home/bett/Documents/c/taskApp/icons/back.png"
+#define REMOVE_ICON "/home/bett/Documents/c/taskApp/icons/remove.png"
+#define RAISE_ICON "/home/bett/Documents/c/taskApp/icons/raise.png"
+
+#define GLOBAL_MARGIN 25.0f
+
+#define INPUT_BUF_SIZE 512
+
+#define SECONDARY_COLOR (LfColor){65, 167, 204, 255} 
+
+#define DA_INIT_CAP 64 
+
+#define SMOOTH_SCROLL false
+
+#define DATE_CMD "date +\"%d.%m.%Y, %H:%M\""
